18086b7b174ed3da6baee81ada6489b99d6f224a
Moiseev Evgeny <moiseevEO@yandex.ru>
2024-04-03 20:52:03 +0300

    commit

--- public/assets/js/files/forms.js
+++ public/assets/js/files/forms.js
@@ -681,6 +681,10 @@ function autoComplete(form) {
 	allFields.forEach(field => {
 		field.setAttribute('autocomplete', 'none');
 		field.setAttribute('readonly', 'readonly');
+
+		if (field.type == 'hidden') {
+			allFields.forEach(field => field.removeAttribute('autocomplete'));
+		}
 	});
 	
 	document.addEventListener('DOMContentLoaded', () => {
